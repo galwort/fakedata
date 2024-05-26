d22f9922dcf67cb6475db03a858cf6e12829477c
galwort <wthomasgorbett@gmail.com>
2024-05-25 22:11:38 -0400

    Pull in topics and rotate through on home page

--- ui/src/app/home/home.page.ts
+++ ui/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { environment } from 'src/environments/environment';
 
 const app = initializeApp(environment.firebase);
@@ -11,6 +11,35 @@ const db = getFirestore(app);
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  topics: string[] = [];
+  currentTopicIndex: number = 0;
+  currentTopic: string = 'nothing... yet';
+
   constructor() {}
+
+  ngOnInit() {
+    this.fetchTopics();
+  }
+
+  async fetchTopics() {
+    const querySnapshot = await getDocs(collection(db, 'topics'));
+    this.topics = querySnapshot.docs.map((doc) => doc.id);
+    this.startTopicRotation();
+  }
+
+  startTopicRotation() {
+    if (this.topics.length > 0) {
+      this.currentTopic = `Fake data on ${this.topics[this.currentTopicIndex]}`;
+      setInterval(() => {
+        this.currentTopicIndex =
+          (this.currentTopicIndex + 1) % this.topics.length;
+        this.currentTopic = `Fake data on ${
+          this.topics[this.currentTopicIndex]
+        }`;
+      }, 5000);
+    } else {
+      this.currentTopic = 'No topics found';
+    }
+  }
 }
