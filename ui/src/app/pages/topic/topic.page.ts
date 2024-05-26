1751a7e66ffe3dc1bdb9fa1aa4c68996b6173b84
galwort <wthomasgorbett@gmail.com>
2024-05-26 13:24:02 -0400

    Pull relevance scores from firestore

--- ui/src/app/pages/topic/topic.page.ts
+++ ui/src/app/pages/topic/topic.page.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from 'firebase/firestore';
+import { environment } from 'src/environments/environment';
+
+const app = initializeApp(environment.firebase);
+const db = getFirestore(app);
 
 @Component({
   selector: 'app-topic',
@@ -8,12 +20,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class TopicPage implements OnInit {
   topic!: string;
+  records: any[] = [];
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       this.topic = params.get('topic')!;
+      this.fetchRecords();
     });
+  }
+
+  async fetchRecords() {
+    const relevanceCollection = collection(db, 'relevance');
+    const q = query(relevanceCollection, where('topic', '==', this.topic));
+    const querySnapshot = await getDocs(q);
+    this.records = querySnapshot.docs.map((doc) => doc.data());
+    console.log(this.records);
   }
 }
