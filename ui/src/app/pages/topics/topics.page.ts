57c8ce460c416f05f0e6123f29bf8206d9aae220
galwort <wthomasgorbett@gmail.com>
2024-05-27 20:32:41 -0400

    Fix timestamps being pulled from firestore

--- ui/src/app/pages/topics/topics.page.ts
+++ ui/src/app/pages/topics/topics.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Timestamp,
+} from 'firebase/firestore';
 import { environment } from 'src/environments/environment';
 
 const app = initializeApp(environment.firebase);
@@ -22,9 +27,26 @@ export class TopicsPage implements OnInit {
 
   async fetchTopics() {
     const querySnapshot = await getDocs(collection(db, 'topics'));
-    this.topics = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    this.topics = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      const insertTime = data['insert_time']?.toDate();
+      const formattedInsertTime = insertTime
+        ? `${
+            insertTime.getMonth() + 1
+          }/${insertTime.getDate()}/${insertTime.getFullYear()}`
+        : '';
+      const modifiedTime = data['modified_time']?.toDate();
+      const formattedModifiedTime = modifiedTime
+        ? `${
+            modifiedTime.getMonth() + 1
+          }/${modifiedTime.getDate()}/${modifiedTime.getFullYear()}`
+        : '';
+      return {
+        id: doc.id,
+        ...data,
+        formattedInsertTime,
+        formattedModifiedTime,
+      };
+    });
   }
 }
