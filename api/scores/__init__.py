0465a7eb92134e927a390b590f2787f727bf49f1
galwort <wthomasgorbett@gmail.com>
2024-06-07 23:31:33 -0400

    Rename mains in inits

--- api/scores/__init__.py
+++ api/scores/__init__.py
@@ -1,10 +1,10 @@
 import azure.functions as func
-from .relevance import main
+from .relevance import main as relevance_main
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        main()
+        relevance_main()
         return func.HttpResponse("Function executed successfully.", status_code=200)
     except Exception as e:
         return func.HttpResponse(str(e), status_code=500)
--- api/topics/__init__.py
+++ api/topics/__init__.py
@@ -1,10 +1,10 @@
 import azure.functions as func
-from .topics import main
+from .topics import main as topics_main
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        main()
+        topics_main()
         return func.HttpResponse("Function executed successfully.", status_code=200)
     except Exception as e:
         return func.HttpResponse(str(e), status_code=500)
